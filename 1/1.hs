e4e4b7f2f6704d05f206fb050c7aae30526f3ee6
Augustus Odena <aqo2@columbia.edu>
2012-04-29 00:57:50 -0400

    more cleanup

--- 1/1.hs
+++ 1/1.hs
@@ -4,6 +4,10 @@ import MyState
 import Stack
 import Util
 
+--given a list of strings that is this entry into the calculator, we can now just
+--map a function over the list that gives the appropriate stateful computation
+--then sequence those computations and execute them - how do we do this? can decide tomorrow.
+
 reactNum :: String -> State [Double] ()
 reactNum string = State $ \stack -> ((),(numify string):stack)
 
