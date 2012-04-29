14d09b468ff15fba14def15ff793f49618874b4f
Augustus Odena <aqo2@columbia.edu>
2012-04-29 12:47:54 -0400

    I have a function that does what we want. Now it's just making the IO and stuff work.

--- 1/1.hs
+++ 1/1.hs
@@ -4,14 +4,25 @@ import MyState
 import Stack
 import Util
 
---given a list of strings that is this entry into the calculator, we can now just
---map a function over the list that gives the appropriate stateful computation
---then sequence those computations and execute them - how do we do this? can decide tomorrow.
+--now we want a process function that processes these lists of strings
+--we want an action that takes a list of strings then runs react on each of them
+--let's try doing it one string at a time
 
-reactNum :: String -> State [Double] ()
-reactNum string = State $ \stack -> ((),(numify string):stack)
+react :: [Char] -> State [Double] Double
+react string
+  | isNum string = reactNum string
+  | otherwise    = reactOp string
 
-reactOp :: (Fractional a) => [Char] -> State (Stack a) a
+
+process strings = do
+  sequence $ map react strings
+
+--reactNum :: (Fractional a) => String -> State (Stack a) a
+reactNum :: String -> State [Double] Double
+reactNum string = State $ \stack -> (k,k:stack)
+  where k = numify string
+
+reactOp :: (Fractional a) => String -> State (Stack a) a
 reactOp string = apply (toOp string)
 
 toOp :: (Fractional a) => [Char] -> a -> a -> a
