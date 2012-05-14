b0c53171ea31eb0f8644325911c45095d287e722
Augustus Odena <aqo2@columbia.edu>
2012-05-14 13:38:39 -0400

    Finished exercise 14.

--- 14/14.hs
+++ 14/14.hs
@@ -1,5 +1,10 @@
-import System.IO
+import System
+import Data.List
 
 main = do
-  args <- getArgs
-  print args
+  [n,fileName] <- getArgs
+  contents <- readFile fileName
+  mapM print $ process (read n :: Int) contents
+
+process n = (take n) . concat . map (nub) . (sortBy p) . group . sort . words
+  where p x y = compare (genericLength y) (genericLength x)
