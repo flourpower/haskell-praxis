9c4d86e5f9ff795067afcb44b394849e34efa348
Augustus Odena <aqo2@columbia.edu>
2012-05-16 14:08:16 -0400

    This isn't going very well. I will come back to the problem afresh tomorrow.

--- 5/5.hs
+++ 5/5.hs
@@ -1,4 +1,17 @@
 --Programming a cyclical list
 import Data.List
 
-f = id
+j n m = helper [] m [1..n]
+
+helper accum m [] = accum
+helper accum m rest = helper (accum ++ [newGuy]) m newRest
+  where newGuy = (cycle rest) !! (m-1)
+        newRest = renew m rest
+
+renew m xs = right ++ left
+  where left = init . (take m) $ xs
+        right = drop m xs
+
+safeTail [] = []
+safeTail [x] = []
+safeTail (x:xs) = xs
