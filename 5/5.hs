f56ae3e50ec8153c63228b03fc28d229e7e0ce6d
Augustus Odena <aqo2@columbia.edu>
2012-05-17 10:45:57 -0400

    Solved 5.

--- 5/5.hs
+++ 5/5.hs
@@ -1,4 +1,5 @@
 --Programming a cyclical list
+--Josephus survives in the 31st position for (41,3)
 import Data.List
 
 j n m = helper [] m [1..n]
@@ -8,10 +9,6 @@ helper accum m rest = helper (accum ++ [newGuy]) m newRest
   where newGuy = (cycle rest) !! (m-1)
         newRest = renew m rest
 
-renew m xs = right ++ left
-  where left = init . (take m) $ xs
-        right = drop m xs
+renew m xs = (takeWhile (/= guy)) . (drop m) . cycle $ xs
+  where guy = (cycle xs) !! (m-1)
 
-safeTail [] = []
-safeTail [x] = []
-safeTail (x:xs) = xs
